a1fc9ac306d6f0b3688ec21f3304a982edfeb7e1
c88772404 <c88772404@gmail.com>
2023-12-12 22:27:02 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,34 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
+public class Item {
+
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구수항 추가
+    private Long id;
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 수정 요구수항 추가
+    private String itemName;
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+//    @NotNull
+    //@Max(9999)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
